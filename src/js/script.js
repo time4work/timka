5f3eb92c4c59d5a51e1f00fda8738bb864d0bb51
Nik Lazariev <nlazariev@softjourn.com>
2019-05-26 00:55:20 +0200

    +1

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
+console.log('timka lib start');
 import Module from './module';
 
-console.log('start');
-const timka = new Module()
+const timka = new Module();
+window.timka = timka;
 
+// console.log('timka',timka);
+// console.log(0)
+//...
+// module.exports.timka = timka;
 // some comment
 // nothing new
